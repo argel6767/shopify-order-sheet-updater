be7a79d966444ce6ba88c0a6bd64733db144ba78
argel6767 <argel6767@gmail.com>
2024-12-05 21:05:57 -0500

    add Shopify GraphQL api call

---
+++ src/api/grabCustomerData.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_SHOPIFY_API_KEY;
+
+const query = "";
+
+
+const grabCustomerData = async () =>  {
+    try {
+        const response = await axios.post('https://your-store.myshopify.com/admin/api/2023-01/graphql.json', {query},
+            {headers: {'Content-Type': 'application/json', 'X-Shopify-Access-Token': API_KEY}});
+        return response.data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export default grabCustomerData;
